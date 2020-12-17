1794daefba4da37eb9720204d8e778799328d195
Shriya <>
2020-12-17 18:12:04 -0500

    1st loop done

--- Sorts.java
+++ Sorts.java
@@ -3,5 +3,15 @@ public class Sorts{
   *@postcondition The array will be modified such that the elements will be in increasing order.
   *@param data  the elements to be sorted.
   */
-  public static void bubbleSort(int[] data){  }
+  public static void bubbleSort(int[] data){
+    int temp = 0;
+      for (int i = 0; i<data.length-1; i++){
+        if (data[i]>data[i+1]){
+          temp = data[i+1];
+          data[i+1]=data[i];
+          data[i]= temp;
+        }
+      }
+   }
+
 }
