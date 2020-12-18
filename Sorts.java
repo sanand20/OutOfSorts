db36e5109c52c994029c0ae982f8d333b8db4cf9
Shriya <>
2020-12-18 13:15:05 -0500

    testing code for selectionsort

--- Sorts.java
+++ Sorts.java
@@ -17,7 +17,18 @@ public class Sorts{
  }
 
  public static void selectionSort(int[] data){
-
+   int temp = 0;
+   for (int i = 0; i <data.length-1; i++){
+     int min = i;
+     for (int j = i; j<data.length-1; j++){
+       if (data[j+1]<data[j]){
+         min = j+1;
+       }
+     }
+     temp = data[min];
+     data[min]= data[i];
+     data[i] = temp;
+   }
   }
 
 }
