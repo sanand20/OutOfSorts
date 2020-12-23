031f885b154b423f341fbcd05c548b5a4b52d028
Shriya <>
2020-12-23 17:00:23 -0500

    everything fixed

--- Sorts.java
+++ Sorts.java
@@ -4,25 +4,25 @@ public class Sorts{
   *@param data  the elements to be sorted.
   */
   public static void bubbleSort(int[] data){
-    int temp = 0;
-      for (int j = 0; j <data.length-1; j++){
-        for (int i = 0; i<data.length-1; i++){
-        if (data[i]>data[i+1]){
-          temp = data[i+1];
-          data[i+1]=data[i];
-          data[i]= temp;
-        }
-      }
-   }
- }
+  boolean sorted = false;
+  for(int j = 0; j<data.length-1 && sorted == false; j++){
+  sorted = true;
+  for (int i = 0; i < data.length - j-1; i++)
+  if (data[i] > data[i + 1]){
+  sorted = false;
+  int temp = data[i];
+  data[i] = data[i + 1];
+  data[i+1] = temp;
+  }
+  }}
 
  public static void selectionSort(int[] data){
    int temp = 0;
-   for (int i = 0; i <data.length-1; i++){
+   for (int i = 0; i <data.length; i++){
      int min = i;
-     for (int j = i; j<data.length-1; j++){
-       if (data[j+1]<data[j]){
-         min = j+1;
+     for (int j = i; j<data.length; j++){
+       if (data[j]<data[min]){
+         min = j;
        }
      }
      temp = data[min];
@@ -30,5 +30,21 @@ public class Sorts{
      data[i] = temp;
    }
   }
+
+
+
+  public static void insertionSort(int[] data){
+ {
+for (int i = 1; i < data.length; ++i) {
+int key = data[i];
+int j = i - 1;
+while (j >= 0 && data[j] > key) {
+data[j + 1] = data[j];
+j = j - 1;
+}
+data[j + 1] = key;
+    }
+  }
+}
 
 }
